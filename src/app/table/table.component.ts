29f3a04b9a9504cf786ffdece05ff66e5bc76f6a
aditya_kk <adityakrishnakumar12@gmail.com>
2022-01-19 13:21:02 +0530

    basic table made in table component

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { User } from "../shared/user.model";
 
 @Component({
     selector : "app-table",
     templateUrl : "./table.component.html",
     styleUrls : ["./table.component.css"]
 })
-export class TableComponent{}
+export class TableComponent{
+    users : User[] = []
+
+    
+}
